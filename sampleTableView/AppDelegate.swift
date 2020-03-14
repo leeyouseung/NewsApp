80c4a2bacd1b24d4427127460153222ec962652c
이유승 <leeyouseng100@gmail.com>
2020-03-15 00:32:35 +0900

    [FIX] iOS Version Error
    iOS 버전 오류 수정

--- sampleTableView/AppDelegate.swift
+++ sampleTableView/AppDelegate.swift
@@ -9,10 +9,9 @@
 import UIKit
 import CoreData
 
+@available(iOS 13.0, *)
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
